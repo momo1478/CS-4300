3d13bddefc5e8b258e439a0ab0488e99edfc2de6
ewaugh <ewaugh@lab1-10.eng.utah.edu>
2017-10-03 13:23:46 -0600

    my make percept sentence and A1

--- A3/CS4300_BR_gen_KB.m
+++ A3/CS4300_BR_gen_KB.m
@@ -58,14 +58,14 @@ for i = 1:16 %sets up rule for breezes where one neighbor must be a pit
     new_string_rule = ['-', vars(i + breeze_offset).var];
     for j = 1:16
         if conGraph(i,j) == 1
-            KBi(length(KBi) + 1).clauses = [-(j + pit_offset), i + breeze_offset];
+            KBi(length(KBi) + 1).clauses = sort([-(j + pit_offset), i + breeze_offset]);
             KB(length(KB) + 1).clauses = ['-', vars(j + pit_offset).var, BLANK, vars(i + breeze_offset).var];
             
             new_rule = [new_rule, j + pit_offset];
             new_string_rule = [new_string_rule, BLANK, vars(j + pit_offset).var];
         end
     end
-    KBi(length(KBi) + 1).clauses = new_rule;
+    KBi(length(KBi) + 1).clauses = sort(new_rule);
     KB(length(KB) + 1).clauses = new_string_rule;
 end
 
@@ -74,14 +74,14 @@ for i = 1:16 %sets up rule for stenches where one neighbor must be a wumpus
     new_string_rule = strcat('-', vars(i + stench_offset).var);
     for j = 1:16
         if conGraph(i,j) == 1
-            KBi(length(KBi) + 1).clauses = [-(j + wumpus_offset), i + stench_offset];
+            KBi(length(KBi) + 1).clauses = sort([-(j + wumpus_offset), i + stench_offset]);
             KB(length(KB) + 1).clauses = ['-', vars(j + wumpus_offset).var, BLANK, vars(i + stench_offset).var];
             
             new_rule = [new_rule, j + wumpus_offset];
             new_string_rule = strcat(new_string_rule, {' '}, vars(j + wumpus_offset).var);
         end
     end
-    KBi(length(KBi) + 1).clauses = new_rule;
+    KBi(length(KBi) + 1).clauses = sort(new_rule);
     KB(length(KB) + 1).clauses = new_string_rule;
 end
 
@@ -94,12 +94,12 @@ for i = 1:16 %only 1 gold on the board rule set
         if i ~= j
             new_rule = [-(i + glitter_offset), -(j + glitter_offset)];
             new_string_rule = ['-', vars(i + glitter_offset).var, BLANK, '-', vars(j + glitter_offset).var];
-            KBi(length(KBi) + 1).clauses = new_rule;
+            KBi(length(KBi) + 1).clauses = sort(new_rule);
             KB(length(KB) + 1).clauses = new_string_rule;
         end
     end
 end
-KBi(length(KBi) + 1).clauses = gold_rule;
+KBi(length(KBi) + 1).clauses = sort(gold_rule);
 KB(length(KB) + 1).clauses = gold_string_rule;
 
 wumpus_rule = [];
@@ -111,24 +111,24 @@ for i = 1:16 %only 1 wumpus on the board rule set
         if i ~= j
             new_rule = [-(i + wumpus_offset),-(j + wumpus_offset)];
             new_string_rule = ['-', vars(i + wumpus_offset).var, BLANK, '-', vars(j + wumpus_offset).var];
-            KBi(length(KBi) + 1).clauses = new_rule;
+            KBi(length(KBi) + 1).clauses = sort(new_rule);
             KB(length(KB) + 1).clauses = new_string_rule;
         end
     end
 end
-KBi(length(KBi) + 1).clauses = wumpus_rule;
+KBi(length(KBi) + 1).clauses = sort(wumpus_rule);
 KB(length(KB) + 1).clauses = wumpus_string_rule;
 
 for i = 1:16 %no gold on a pit
     new_rule = [-(i + glitter_offset), -(i + pit_offset)];
     new_string_rule = ['-', vars(i + glitter_offset).var, BLANK, '-', vars(i + pit_offset).var];
-    KBi(length(KBi) + 1).clauses = new_rule;
+    KBi(length(KBi) + 1).clauses = sort(new_rule);
     KB(length(KB) + 1).clauses = new_string_rule;
 end
 
 for i = 1:16 %no wumpus on a pit
     new_rule = [-(i + wumpus_offset), -(i + pit_offset)];
     new_string_rule = ['-', vars(i + wumpus_offset).var, BLANK, '-', vars(i + pit_offset).var];
-    KBi(length(KBi) + 1).clauses = new_rule;
+    KBi(length(KBi) + 1).clauses = sort(new_rule);
     KB(length(KB) + 1).clauses = new_string_rule;
 end
---
+++ A4/A4b/CS4300_make_percept_sentence.m
@@ -0,0 +1,56 @@
+function sentence = CS4300_make_percept_sentence(percept,x,y)
+% CS4300_make_percept_sentence - create logical sentence from percept
+% On input:
+%   percept (1x5 Boolean vector): percept
+%       [Stench,Breeze,Glitter,Scream,Bump]
+%   x (int): x location of agent
+%   y (int): y location of agent
+% On output:
+%   sentence (KB struct): logical sentence (CNF)
+%       (1).clauses (int): c1 (index of Sxy if stench), else -c1
+%       (2).clauses (int): c2 (index of Bxy if breeze), else -c2
+%       (3).clauses (int): c3 (index of Gxy if glitter), else -c3
+%       (4).clauses (int): c4 (index of Cxy if scream), else -c4
+%       (5).clauses (int): c5 (index of Exy if bump), else -c5
+% Call:
+%   s = CS4300_make_percept_sentence([0,1,0,0,0],3,2);
+% Author:
+%   Eric Waugh and Monish Gupta
+%   u0947296 and u1008121
+%   Fall 2017
+%
+
+breeze_offset = 0;
+glitter_offset = 16;
+pit_offset = 32;
+stench_offset = 48;
+wumpus_offset = 64;
+location = (x - 1) * 4 + y;
+
+sentence = [];
+
+%stench case
+if percept(1) == 0
+    sentence(1).clauses = -1 * (location + stench_offset);
+else
+    sentence(1).clauses = location + stench_offset;
+end
+
+%breeze case
+if percept(2) == 0
+    sentence(2).clauses = -1 * (location + breeze_offset);
+else
+    sentence(2).clauses = location + breeze_offset;
+end
+
+%glitter case
+if percept(3) == 0
+    sentence(3).clauses = -1 * (location + glitter_offset);
+else
+    sentence(3).clauses = location + glitter_offset;
+end
+
+sentence(4).clauses = [];
+sentence(5).clauses = [];
+
+end
