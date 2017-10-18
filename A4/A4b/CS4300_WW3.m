ff37e69e07ca868457b639ab8661b7f5a42dd7f3
ewaugh <ewaugh@lab1-17.eng.utah.edu>
2017-10-17 19:15:25 -0600

    new plan shot helper functions

--- A4/A4b/CS4300_WW3.m
+++ A4/A4b/CS4300_WW3.m
@@ -27,16 +27,16 @@ agent.dir = 0;  % facing right
 agent.succeed = 0;  % has gold and climbed out
 agent.climbed = 0; % climbed out
 
-% % clear(f_name);
-% % 
-% % board1 = [0,0,0,3;...
-% %           0,0,0,0;...
-% %           2,1,0,0;...
-% %           0,0,0,0];
-% % [score,trace] = CS4300_WW1(max_steps,f_name,board1);
-% % scores(1).board = board1;
-% % scores(1).score = score;
-% % scores(1).trace = trace;
+clear(f_name);
+
+board1 = [0,0,0,3;...
+          0,0,0,0;...
+          2,1,0,0;...
+          0,0,0,0];
+[score,trace] = CS4300_WW1(max_steps,f_name,board1);
+scores(1).board = board1;
+scores(1).score = score;
+scores(1).trace = trace;
 
 clear(f_name);
 
@@ -50,12 +50,12 @@ scores(2).score = score;
 scores(2).trace = trace;
  
 clear(f_name);
-% % 
-% % board3 = [0,0,0,0;...
-% %     0,0,0,0;...
-% %     3,2,0,0;...
-% %     0,1,0,0];
-% % [score,trace] = CS4300_WW1(max_steps,f_name,board3);
-% % scores(3).board = board3;
-% % scores(3).score = score;
-% % scores(3).trace = trace;
+
+board3 = [0,0,0,0;...
+    0,0,0,0;...
+    3,2,0,0;...
+    0,1,0,0];
+[score,trace] = CS4300_WW1(max_steps,f_name,board3);
+scores(3).board = board3;
+scores(3).score = score;
+scores(3).trace = trace;
---
+++ A4/A4b/CS4300_turn_plan.m
@@ -0,0 +1,7 @@
+function [ output_args ] = CS4300_turn_plan( input_args )
+%CS4300_TURN_PLAN Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
