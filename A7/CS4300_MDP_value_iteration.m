833808f8767fe3f79015ede378bf2a20facc1753
monishg <monishg@lab1-34.eng.utah.edu>
2017-11-16 20:25:32 -0700

    idk

--- A7/CS4300_MDP_value_iteration.m
+++ A7/CS4300_MDP_value_iteration.m
@@ -45,13 +45,15 @@ max_utility_change = 0;
 U = zeros(1,16);
 newU = zeros(1,16);
 
-U_trace = zeros(1,16);
+U_trace = [];
 
 n = size(S,1);
 k = size(A,1);
 
 while ~first_iter && max_utility_change < (1 - gamma)/gamma
 max_utility_change = 0;
+
+U_trace = [U_trace;newU];
 U = newU;
 
 %for each state s in S do
@@ -60,6 +62,7 @@ for s = 1 : n
     
     tempNewUs = zeroes(1,n);
     tempNewUs(1:n) = newU(s);
+    
     bestUtil = -10000;
     for a = 1 : k
       bestUtil = max(bestUtil , max( times(P(s,a).probs ,tempNewUs) ) );
