432bc89c7289dbab5fb0a36e59d0e65363f853c8
monishg <monishg@lab1-21.eng.utah.edu>
2017-11-20 23:20:12 -0700

    done policy

--- A7/CS4300_MDP_policy.m
+++ A7/CS4300_MDP_policy.m
@@ -17,3 +17,24 @@ function policy = CS4300_MDP_policy(S,A,P,U)
 %    u0947296 and u1008121
 %    Fall 2017
 
+policy = zeros(1,length(S));
+
+for s = 1 : length(S)
+    bestAction = 0;
+    bestSum = -Inf;
+    for a = 1 : length(A)
+        findI = find( P(s,a).probs );
+        add = 0;
+        
+        for i = 1 : length(findI)
+            add = add + (P(s,a).probs(findI(i)) * U(findI(i)));
+        end
+        
+        if add > bestSum
+            bestAction = a;
+            bestSum = add;
+        end
+    end
+    policy(s) = bestAction;
+end
+
