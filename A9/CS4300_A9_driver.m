681eab1b7e527ac8eaf67cd7140f3c3176889aec
ewaugh <ewaugh@lab1-32.eng.utah.edu>
2017-12-09 15:17:24 -0700

    image stuff fixed from Tom email

--- A9/CS4300_A9_driver.m
+++ A9/CS4300_A9_driver.m
@@ -14,32 +14,34 @@ load('G.mat');
 
 size = 9;
 
-X = [];
+Xim = [];
 y = [];
 
 for i = 1:size
    im = G(i).im;
    im = imresize(im,[15,15]);
-   im = im > 50;
-   X(i,:) = im(:);
+   im = im > 220;
+   Xim(i,:) = im(:,8)';
    y(i) = 1;
 end
 
 for i = 1:size
    im = W(i).im;
    im = imresize(im,[15,15]);
-   im = im > 50;
-   X(i + 9,:) = im(:);
-   y(i + 9) = 1;
+   im = im > 220;
+   Xim(i + 9,:) = im(:,8)';
+   y(i + 9) = 2;
 end
 
 for i = 1:size
    im = P(i,1).im;
    im = imresize(im,[15,15]);
-   im = im > 50;
-   X(i + 18,:) = im(:);
-   y(i + 18) = 1;
+   im = im > 220;
+   Xim(i + 18,:) = im(:,8)';
+   y(i + 18) = 3;
 end
+
+[w,pc] = CS4300_perceptron_learning(Xim,y==1,0.1,1000,0);
 
 end
 
